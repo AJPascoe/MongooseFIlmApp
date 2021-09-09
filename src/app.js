73ea4ab4d2fb02ee4941e75a6e89ec2b6a2548ab
AJPascoe <pascoe312@gmail.com>
2021-09-09 11:06:14 +0100

    creating add function in app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
-// require("./db/connection");
+require("./db/connection");
+const mongoose = require("mongoose");
+const {addMovie} = require ("./films/film.methods");
+const command = process.argv[2];
+
+const app = async ()=> {
+    if (command === "add"){
+        await addMovie({
+            name: process.argv[3],
+            actor: process.argv[4],
+            like: process.argv[5]
+        });
+    }
+    mongoose.disconnect()
+};
+
+app();
 
