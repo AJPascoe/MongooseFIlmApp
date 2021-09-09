3c3b9e9cdc598ccd552b9a6cde1bf51d9d09f266
AJPascoe <pascoe312@gmail.com>
2021-09-09 11:06:54 +0100

    setting up model.js

---
+++ src/films/film.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const filmSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true // checks to see if it already exist in database to avoid duplicates
+    },
+    actor: {
+        type: String
+    },
+    like: {
+        type: String,
+        required: true,
+    },
+});
+
+const Film = mongoose.model("Film", filmSchema); // assigns schema to a name.
+
+module.exports = Film;
